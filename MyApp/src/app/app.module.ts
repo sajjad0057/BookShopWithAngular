e9a7276ec1e7b5d39f3b97d49cd87b6b89bc8ab2
sajjad0057 <sajjadhossain0057@gmail.com>
2023-03-07 04:44:16 +0600

    property binding

--- MyApp/src/app/app.module.ts
+++ MyApp/src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { BooksComponent } from './books/books.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+@NgModule(
+    {
+        declarations: [AppComponent, BooksComponent],
+        imports: [BrowserModule],
+        bootstrap: [AppComponent]
+    }
+)
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+export class AppModule{
+
+}
---
+++ MyApp/src/app/books/books.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent implements OnInit {
+  
+
+
+
+  name : string = "c# basic";
+  author : string = "Robert C Martin"
+  src : string = "https://m.media-amazon.com/images/I/61yRxqI4WfL._AC_UY218_.jpg"
+
+  name2 : string = "c# Intermediate";
+  author2 : string = "Robert C Martin"
+  src2 : string = "https://m.media-amazon.com/images/I/71s6gwpezHL._AC_UY218_.jpg"
+
+  isDesabled : boolean = true;
+
+  constructor(){}
+  
+  ngOnInit(): void {
+    
+  }
+
+}
